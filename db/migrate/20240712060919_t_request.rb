98203cf5cfb28b619fbb3fa6dccbdbbc53ead175
satoru-kitajima <a.fridge.on.heatisland@gmail.com>
2024-09-10 23:44:50 +0000

    migrationファイルを修正

--- db/migrate/20240712060919_t_request.rb
+++ db/migrate/20240712060919_t_request.rb
@@ -1,5 +1,5 @@
-class TRequest < ActiveRecord::Migration[6.1]
-  def change
-    rename :t_requests, :requests
-  end
-end
+# class TRequest < ActiveRecord::Migration[6.1]
+#   def change
+#     rename :t_requests, :requests
+#   end
+# end
