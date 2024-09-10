48ff60c9b5ab10f126303c20f03b852b2ce8d776
satoru-kitajima <a.fridge.on.heatisland@gmail.com>
2024-09-10 23:50:21 +0000

    migration修正その２

--- db/migrate/20240712060254_rename_requests_to_t_requests.rb
+++ db/migrate/20240712060254_rename_requests_to_t_requests.rb
@@ -1,5 +1,5 @@
-class RenameRequestsToTRequests < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :requests, :t_requests
-  end
-end
+# class RenameRequestsToTRequests < ActiveRecord::Migration[6.1]
+#   def change
+#     rename_table :requests, :t_requests
+#   end
+# end
--- db/migrate/20240712064513_m_train_fix.rb
+++ db/migrate/20240712064513_m_train_fix.rb
@@ -1,5 +1,5 @@
-class MTrainFix < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :m_trains, :trains
-  end
-end
+# class MTrainFix < ActiveRecord::Migration[6.1]
+#   def change
+#     rename_table :m_trains, :trains
+#   end
+# end
