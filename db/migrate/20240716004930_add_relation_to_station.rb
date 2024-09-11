3bebf0b65a287f485a04ecb33f21f3cdebe18b17
satoru-kitajima <a.fridge.on.heatisland@gmail.com>
2024-09-11 05:29:01 +0000

    bigintを修正

--- db/migrate/20240716004930_add_relation_to_station.rb
+++ db/migrate/20240716004930_add_relation_to_station.rb
@@ -1,5 +1,7 @@
 class AddRelationToStation < ActiveRecord::Migration[6.1]
   def change
+    change_column :trains, :departure_station_id, :bigint
+    change_column :trains, :arrival_station_id, :bigint
     add_foreign_key :trains, :stations, column: :departure_station_id
     add_foreign_key :trains, :stations, column: :arrival_station_id
   end
