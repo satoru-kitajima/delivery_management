2f1d7d5b86a819d9b4e5f17219dcdc4ae349bc9c
satoru-kitajima <a.fridge.on.heatisland@gmail.com>
2024-09-11 06:47:17 +0000

    migration修正

--- db/migrate/20240911064028_update_preferred_shipment_to_trains_in_requests.rb
+++ db/migrate/20240911064028_update_preferred_shipment_to_trains_in_requests.rb
@@ -1,13 +1,15 @@
 class UpdatePreferredShipmentToTrainsInRequests < ActiveRecord::Migration[6.1]
   def change
-     # preferred_shipment_idをtrains_idに変更
-     rename_column :requests, :preferred_shipment_id, :train_id
+    # preferred_shipmentカラムを削除
+    remove_column :requests, :preferred_shipment, :bigint
     
-     # 外部キー制約をtrainsテーブルに接続
-     remove_foreign_key :requests, column: :preferred_shipment_id
-     add_foreign_key :requests, :trains, column: :train_id
- 
-     # preferred_shipmentカラムを削除
-     remove_column :requests, :preferred_shipment, :bigint
+    # 外部キー制約を削除
+    remove_foreign_key :requests, column: :preferred_shipment_id
+
+    # preferred_shipment_idをtrain_idに変更
+    rename_column :requests, :preferred_shipment_id, :train_id
+
+    # 外部キー制約をtrainsテーブルに接続
+    add_foreign_key :requests, :trains, column: :train_id
   end
 end
